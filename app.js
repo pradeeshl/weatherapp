df713ffbd50af2a09e5ae0a73ee6d9650176b522
Vijay Ganesh S <144366222+vg006@users.noreply.github.com>
2024-09-06 17:16:16 +0530

    [FEAT] First prototype created 👍

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-const express=require("express");
-const app=express();
-const path=require("path");
-const bodyparser=require("body-parser");
-//const dbo=require('./db)
+const express= require("express");
+const app= express();
+const path= require("path");
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set views directory
+app.set('views', path.join(__dirname, 'views'));
+
 // home page middleware
-app.use('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'..','weatherapp','views','html_response_page.html'));
-
+app.get('/',(req,res)=>{
+    const filePath = path.join(__dirname,'views','homePage.ejs');
+    res.render(filePath,{pageTitle:'Home Page'});
 })
-// app.use('location',(req,res,next)=>{
-    
-// })
-// app.get('/',async (req,res)=>{
-// let database=await dbo.getDatabase();
-// const collection=database.collection(collection_name );})
 
-// const cursor=collection.find({});
-// let authors=await cursor.toArray();
+app.get('/:location',(req,res,next)=>{
+    const location = req.params.location;
 
+    // logic to get data from database
+    // app.get('/',async (req,res)=>{
+    // let database=await dbo.getDatabase();
+    // const collection=database.collection(collection_name );})
 
+    // const cursor=collection.find({});
+    // let authors=await cursor.toArray();
+
+    const filePath = path.join(__dirname,'views',`detailsPage.ejs`);
+    res.render(filePath,{location:location});
+})
 
 app.listen(8000,()=>{console.log("Listening to port 8000");})
